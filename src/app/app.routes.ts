317b582cd8edc6f623adf86268271fefa6b6d424
sanketp2001 <sanket.p@angularminds.in>
2025-03-01 17:34:47 +0530

    added routes and fontawesome cdn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CandidateComponent } from './pages/candidate/candidate.component';
 import { InterviewerComponent } from './pages/interviewer/interviewer.component';
+import { ChallengeComponent } from './pages/challenge/challenge.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'interviewer', pathMatch: 'full' },
-  { path: 'candidate', component: CandidateComponent },
+  { path: 'candidate/:id', component: CandidateComponent },
   { path: 'interviewer', component: InterviewerComponent },
+  { path: 'challenge/:id', component: ChallengeComponent },
   { path: '**', redirectTo: 'candidates' },
 ];
