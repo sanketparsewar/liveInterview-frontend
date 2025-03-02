d3591dd188f896b4e0ffa0d8cb768439918bfc31
Sanket Parsewar <sanket.parsewar97@gmail.com>
2025-03-02 18:21:55 +0530

    updated project component

--- src/app/core/modalComponents/project/project.component.ts
+++ src/app/core/modalComponents/project/project.component.ts
@@ -1,20 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AlertService } from './../../services/alert/alert.service';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ProjectService } from './../../services/project/project.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css',
 })
-export class ProjectComponent {
-  project: any;
+export class ProjectComponent implements OnInit {
+  projectForm!:FormGroup;
   @Output() toggleProjectModal = new EventEmitter();
-  constructor() {}
+  @Output() getProjectList = new EventEmitter();
+  constructor(private fb:FormBuilder,private projectService:ProjectService,private alertService:AlertService) {}
 
-  createProject() {
-    console.log('Creating session:', this.project);
-    this.ontoggleProjectModal();
+  ngOnInit() {
+    this.projectForm = this.fb.group({
+      title: ['', [Validators.required]],
+      skills:['', [Validators.required]],
+      projectUrl:['', [Validators.required]],
+    })
   }
+  createProject(projectForm: FormGroup) {
+    this.projectService.createProject(projectForm.value).subscribe({
+      next: (res) => {
+        this.alertService.showSuccess('Project created successfully');
+        this.getProjectList.emit()
+        this.ontoggleProjectModal();
+      },
+      error: (error) => {
+        this.alertService.showError(error.error.message);
+        // console.error('Error creating session:', error);
+      },
+    })
+  }
+
 
   ontoggleProjectModal() {
     this.toggleProjectModal.emit(false);
