f5378d5d458994e8bb596462ff7c78087e79cdd9
sanketp2001 <sanket.p@angularminds.in>
2025-03-03 18:33:12 +0530

    auth services

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private BASE_URL = " https://admin.liveexamcenter.in/api";
+
+  constructor(private http: HttpClient) { }
+
+  login(loginData: any) {
+    return this.http.post(`${this.BASE_URL}/auth/login`, { email: loginData.email, password: loginData.password, skipCaptcha: true });
+  }
+}
