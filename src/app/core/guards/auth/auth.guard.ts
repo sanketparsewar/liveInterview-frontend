90b223463e5a4770b089c39b2010a54268b65e5c
sanketp2001 <sanket.p@angularminds.in>
2025-03-03 18:34:15 +0530

    guards

---
+++ src/app/core/guards/auth/auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return false;
+  }
+  return true;
+};
