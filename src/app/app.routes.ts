fe2ad95b81fe0472286203c434d0cb0591b693db
sanketp2001 <sanket.p@angularminds.in>
2025-03-03 18:34:35 +0530

    added login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { CandidateComponent } from './pages/candidate/candidate.component';
 import { InterviewerComponent } from './pages/interviewer/interviewer.component';
 import { ChallengeComponent } from './pages/challenge/challenge.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'interviewer', pathMatch: 'full' },
-  { path: 'candidate/:id', component: CandidateComponent },
   { path: 'interviewer', component: InterviewerComponent },
+  { path: 'candidate/:id', component: CandidateComponent },
   { path: 'challenge/:id', component: ChallengeComponent },
+
+  {path:'login',component:LoginComponent},
+
+
   { path: '**', redirectTo: 'candidates' },
 ];
