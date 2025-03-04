f0dac7b59eb9f09cf04b185a613b25bb989d0567
sanketp2001 <sanket.p@angularminds.in>
2025-03-04 14:19:06 +0530

    updates

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,18 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private BASE_URL = " https://admin.liveexamcenter.in/api";
-
+//  userSubject = new BehaviorSubject<any>({});
+//   user$ = this.userSubject.asObservable();
   constructor(private http: HttpClient) { }
 
   login(loginData: any) {
     return this.http.post(`${this.BASE_URL}/auth/login`, { email: loginData.email, password: loginData.password, skipCaptcha: true });
   }
   getLoggedInterviewer(){
-    // return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse(localStorage.getItem('user')|| '');
   }
 }
