14828f14878ea5cd26b1a8bb71e050b4d73e66bb
sanketp2001 <sanket.p@angularminds.in>
2025-03-04 10:19:39 +0530

    guards for preventing the direct url access with out login

--- src/app/core/guards/interviewer/interviewer.guard.ts
+++ src/app/core/guards/interviewer/interviewer.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const interviewerGuard: CanActivateFn = (route, state) => {
-  return true;
+  const token = localStorage.getItem('token');
+  const router=inject(Router)
+  if (token) {
+    return true;
+  } else {
+    router.navigateByUrl('/login')
+    return false;
+  }
 };
