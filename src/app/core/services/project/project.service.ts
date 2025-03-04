35a96bcfab762153ab254264c0e8858ef729de58
sanketp2001 <sanket.p@angularminds.in>
2025-03-04 15:38:05 +0530

    updates

--- src/app/core/services/project/project.service.ts
+++ src/app/core/services/project/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment/environment.prod';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IProject } from '../../models/interfaces/project.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -19,7 +20,7 @@ export class ProjectService {
   getProjectById(id: string): Observable<any> {
     return this.http.get<any>(`${this.BASE_URL}/project/${id}`);
   }
-  updateProjectById(id: string, project: any): Observable<any> {
+  updateProjectById(id: string, project: IProject): Observable<any> {
     return this.http.put<any>(`${this.BASE_URL}/project/${id}`, project);
   } 
   deleteProjectById(id: string): Observable<any> {
