08e07e86e1052b5a6969a9b0e81b6e704575c249
sanketp2001 <sanket.p@angularminds.in>
2025-03-04 10:19:51 +0530

    added guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import { CandidateComponent } from './pages/candidate/candidate.component';
 import { InterviewerComponent } from './pages/interviewer/interviewer.component';
 import { ChallengeComponent } from './pages/challenge/challenge.component';
 import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './core/guards/auth/auth.guard';
+import { interviewerGuard } from './core/guards/interviewer/interviewer.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'interviewer', pathMatch: 'full' },
-  { path: 'interviewer', component: InterviewerComponent },
-  { path: 'candidate/:id', component: CandidateComponent },
-  { path: 'challenge/:id', component: ChallengeComponent },
+  { path: 'interviewer', component: InterviewerComponent,canActivate:[interviewerGuard]},
+  { path: 'candidate/:id', component: CandidateComponent,canActivate:[interviewerGuard]},
+  { path: 'challenge/:id', component: ChallengeComponent,canActivate:[interviewerGuard]},
 
-  {path:'login',component:LoginComponent},
+  {path:'login',component:LoginComponent,canActivate:[authGuard]},
 
 
   { path: '**', redirectTo: 'candidates' },
