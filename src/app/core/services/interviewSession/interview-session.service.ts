a450691a4a3422c5438a2baccd3fe067e7f8fc22
sanketp2001 <sanket.p@angularminds.in>
2025-03-05 10:36:45 +0530

    updates in service

--- src/app/core/services/interviewSession/interview-session.service.ts
+++ src/app/core/services/interviewSession/interview-session.service.ts
@@ -1,33 +1,35 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment/environment.prod';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IinterviewSession } from '../../models/interfaces/interviewSession.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InterviewSessionService {
   private BASE_URL = environment.API_URL;
-  constructor(private http: HttpClient) {}
 
-  createInterviewSession(session: any): Observable<any> {
-    return this.http.post<any>(`${this.BASE_URL}/interviewsession`, session);
-  }
+  constructor(private http: HttpClient) { }
 
-  getAllInterviewSessions(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.BASE_URL}/interviewsession`);
+  createInterviewSession(session: IinterviewSession): Observable<IinterviewSession> {
+    return this.http.post<IinterviewSession>(`${this.BASE_URL}/interviewsession`, session);
   }
 
-  getInterviewSessionById(id: string): Observable<any> {
-    return this.http.get<any>(`${this.BASE_URL}/interviewsession/${id}`);
+  getAllInterviewSessions(): Observable<IinterviewSession[]> {
+    return this.http.get<IinterviewSession[]>(`${this.BASE_URL}/interviewsession`);
   }
-  updateInterviewSessionStatus(id: string): Observable<any> {
-    return this.http.put<any>(`${this.BASE_URL}/interviewsession/sessionstatus/${id}`, {});
+
+  getInterviewSessionById(id: string): Observable<IinterviewSession> {
+    return this.http.get<IinterviewSession>(`${this.BASE_URL}/interviewsession/${id}`);
   }
-  updateInterviewSessionById(id: string): Observable<any> {
+  updateInterviewSessionStatus(id: string): Observable<IinterviewSession> {
+    return this.http.put<IinterviewSession>(`${this.BASE_URL}/interviewsession/sessionstatus/${id}`, {});
+  }
+  updateInterviewSessionById(id: string): Observable<IinterviewSession> {
     return this.http.put<any>(`${this.BASE_URL}/interviewsession/${id}`, {});
   }
-  deleteInterviewSessionById(id: string): Observable<any> {
-    return this.http.delete<any>(`${this.BASE_URL}/interviewsession/${id}`);
+  deleteInterviewSessionById(id: string): Observable<IinterviewSession> {
+    return this.http.delete<IinterviewSession>(`${this.BASE_URL}/interviewsession/${id}`);
   }
 }
