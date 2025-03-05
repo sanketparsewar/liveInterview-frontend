2ab9c5cc984c0f444bc729a44c719e8d100c978c
sanketp2001 <sanket.p@angularminds.in>
2025-03-05 10:37:28 +0530

    updates

--- src/app/core/services/interviewSession/interview-session.service.ts
+++ src/app/core/services/interviewSession/interview-session.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment/environment.prod';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IinterviewSession } from '../../models/interfaces/interviewSession.interface';
 
 @Injectable({
