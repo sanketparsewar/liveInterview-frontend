8527d1581ce7a34560085b10813d651818143878
sanketp2001 <sanket.p@angularminds.in>
2025-03-05 15:18:44 +0530

    updates

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
+  // SOCKET_URL: 'http://192.168.0.84:8000',
+  // API_URL: 'http://192.168.0.84:8000/api',
   SOCKET_URL: 'http://localhost:8000',
   API_URL: 'http://localhost:8000/api',
 };
