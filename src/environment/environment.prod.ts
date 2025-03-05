0b7baba995cd9e85ca7f5e2fb341ce84b4b9c369
sanketp2001 <sanket.p@angularminds.in>
2025-03-05 15:21:51 +0530

    hosted backend url

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   // SOCKET_URL: 'http://192.168.0.84:8000',
   // API_URL: 'http://192.168.0.84:8000/api',
-  SOCKET_URL: 'http://localhost:8000',
-  API_URL: 'http://localhost:8000/api',
+  // SOCKET_URL: 'http://localhost:8000',
+  // API_URL: 'http://localhost:8000/api',
+  SOCKET_URL: 'https://liveinterview-backend.onrender.com',
+  API_URL: 'https://liveinterview-backend.onrender.com/api',
 };
