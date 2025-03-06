1aeee928b0fd121299a854d25a4175c7df9185fe
sanketp2001 <sanket.p@angularminds.in>
2025-03-06 10:17:48 +0530

    updates

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // SOCKET_URL: 'http://192.168.0.84:8000',
-  // API_URL: 'http://192.168.0.84:8000/api',
   // SOCKET_URL: 'http://localhost:8000',
   // API_URL: 'http://localhost:8000/api',
   SOCKET_URL: 'https://liveinterview-backend.onrender.com',
