c3d56d09b23a442c13ae687e971bf2874e1d0ebf
sanketp2001 <sanket.p@angularminds.in>
2025-03-11 18:15:12 +0530

    decode token

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment/environment.prod';
-
+import { jwtDecode } from 'jwt-decode';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private AUTH_URL = environment.AUTH_URL;
   constructor(private http: HttpClient) { }
+
+  getDecodedToken() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      return jwtDecode(token);
+    }
+    return null;
+  }
 
   login(loginData: any) {
     return this.http.post(`${this.AUTH_URL}/auth/login`, { email: loginData.email, password: loginData.password, skipCaptcha: true });
