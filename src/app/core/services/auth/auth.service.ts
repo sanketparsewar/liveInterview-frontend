521a48de2bdece83bba997b9ffc8f4bda3f6f0e0
sanketp2001 <sanket.p@angularminds.in>
2025-03-11 15:34:19 +0530

    added auth url in environment

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { environment } from '../../../../environment/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private BASE_URL = " https://admin.liveexamcenter.in/api";
+  private AUTH_URL = environment.AUTH_URL;
   constructor(private http: HttpClient) { }
 
   login(loginData: any) {
-    return this.http.post(`${this.BASE_URL}/auth/login`, { email: loginData.email, password: loginData.password, skipCaptcha: true });
+    return this.http.post(`${this.AUTH_URL}/auth/login`, { email: loginData.email, password: loginData.password, skipCaptcha: true });
   }
   getLoggedInterviewer() {
     return JSON.parse(localStorage.getItem('user') || '');
--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -4,4 +4,6 @@ export const environment = {
   // API_URL: 'http://localhost:8000/api',
   SOCKET_URL: 'https://liveinterview-backend.onrender.com',
   API_URL: 'https://liveinterview-backend.onrender.com/api',
+  AUTH_URL :" https://admin.liveexamcenter.in/api"
+
 };
