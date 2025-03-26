3148e22c0a331704fb04bfd20867fee75cddf18e
sanketp2001 <sanket.p@angularminds.in>
2025-03-26 11:56:30 +0530

    added toast

--- src/app/core/components/web-camera/web-camera.component.ts
+++ src/app/core/components/web-camera/web-camera.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { WebcamModule, WebcamUtil } from 'ngx-webcam';
+import { AlertService } from '../../services/alert/alert.service';
 @Component({
   selector: 'app-web-camera',
   imports: [WebcamModule, FormsModule, CommonModule],
@@ -12,12 +13,14 @@ export class WebCameraComponent {
   @Input() showWebcam: boolean = false;
   multipleWebcamsAvailable: boolean = false;
 
+  constructor(private alertService: AlertService) { }
+
   ngOnInit(): void {
     WebcamUtil.getAvailableVideoInputs()
       .then((mediaDevices: MediaDeviceInfo[]) => {
         this.multipleWebcamsAvailable = mediaDevices.length > 1;
       })
-      .catch(err => console.error('Error accessing webcams:', err));
+      .catch(err => this.alertService.showError(err));
   }
 
   toggleWebcam(): void {
